060ecd3e6af0df8c0092b41a6d92d5163a6db4fa
Travis Swicegood <development@domain51.com>
2009-07-27 16:05:31 -0500

    handle cases where the page is visited after the fact

--- seeder/views.py
+++ seeder/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, redirect
 from oauthtwitter import OAuthApi
 from seeder.models import AuthorizedAccount, Token, Seeder
 from datetime import datetime
+from urllib2 import HTTPError
 
 def index(request):
     return render_to_response(
@@ -23,33 +24,31 @@ def signup(request):
 def finish(request):
     # TODO: refactor this out -- should be in a util class or on the model
     # TODO: add check for this, redirect if not found
-    request_token = request.session['twitter_request_token']
-    oauth = OAuthApi(settings.TWITTER['CONSUMER_KEY'], settings.TWITTER['CONSUMER_SECRET'], request_token)
-    access_token = oauth.getAccessToken()
-    
-    twitter = OAuthApi(settings.TWITTER['CONSUMER_KEY'], settings.TWITTER['CONSUMER_SECRET'], access_token)
-    user_info = twitter.GetUserInfo()
+    try:
+        request_token = request.session['twitter_request_token']
+        oauth = OAuthApi(settings.TWITTER['CONSUMER_KEY'], settings.TWITTER['CONSUMER_SECRET'], request_token)
+        access_token = oauth.getAccessToken()
 
-    default_account = AuthorizedAccount.objects.default_account()
+        twitter = OAuthApi(settings.TWITTER['CONSUMER_KEY'], settings.TWITTER['CONSUMER_SECRET'], access_token)
+        user_info = twitter.GetUserInfo()
 
-    s = Seeder(
-        twitter_id = user_info.id,
-        twitter_username = user_info.screen_name,
-        authorized_for = default_account
-    )
-    s.set_expires_on_in_days(request.session['duration'])
-    s.save()
+        default_account = AuthorizedAccount.objects.default_account()
 
-    Token.objects.create(
-        seeder = s,
-        oauth_token = access_token.key,
-        oauth_token_secret = access_token.secret
-    )
+        s = Seeder(
+            twitter_id = user_info.id,
+            twitter_username = user_info.screen_name,
+            authorized_for = default_account
+        )
+        s.set_expires_on_in_days(request.session['duration'])
+        s.save()
 
-    return render_to_response(
-        'seeder/finish.html',
-        {
-            'user_info': user_info,
-        }
-    )
+        Token.objects.create(
+            seeder = s,
+            oauth_token = access_token.key,
+            oauth_token_secret = access_token.secret
+        )
+    except HTTPError:
+        pass
+
+    return render_to_response('seeder/finish.html')
 
